31fcf27c5aadba3064ab78f405096d392d209690
folkert van heusden <mail@vanheusden.com>
2024-04-21 22:18:57 +0200

    handle new log levels logging output

--- log.cpp
+++ log.cpp
@@ -134,7 +134,7 @@ void dolog(const log_level_t ll, const char *fmt, ...)
 #endif
 		char *ts_str = nullptr;
 
-		const char *const ll_names[] = { "debug  ", "info   ", "warning", "error  " };
+		const char *const ll_names[] = { "emerg  ", "alert  ", "crit   ", "error  ", "warning", "notice ", "info   ", "debug  ", "none   " };
 
 		asprintf(&ts_str, "%04d-%02d-%02d %02d:%02d:%02d.%06d] %s ",
 				tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, int(now % 1000000),
