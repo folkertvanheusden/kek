06494afd403a172e76a630ee8c157d3b4c1bc59e
folkert van heusden <mail@vanheusden.com>
2024-05-14 07:58:24 +0200

    cpu serialization fix

--- cpu.h
+++ cpu.h
@@ -115,10 +115,8 @@ public:
 	explicit cpu(bus *const b, std::atomic_uint32_t *const event);
 	~cpu();
 
-#if IS_POSIX
 	JsonDocument serialize();
 	static cpu *deserialize(const JsonDocument j, bus *const b, std::atomic_uint32_t *const event);
-#endif
 
 	std::optional<std::string> check_breakpoint();
 	int set_breakpoint(breakpoint *const bp);
