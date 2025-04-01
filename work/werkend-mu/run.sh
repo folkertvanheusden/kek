70cc1e30ac85bde1bbbc195423716ac6cfd668eb
Folkert van Heusden <mail@vanheusden.com>
2025-04-01 12:28:15 +0200

    logfiles

--- work/werkend-mu/run.sh
+++ work/werkend-mu/run.sh
@@ -3,12 +3,13 @@
 echo boot
 echo 'rl(0,0)unix'
 
-rm ~/temp/ramdisk/test2.log
+LF=~/temp/ramdisk/test2.log
+rm $LF
 
 if [ "$1" = "fast" ] ; then
-./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -S 256 -L error,info -l ~/temp/ramdisk/test2.log -P -R rl02
+./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -S 256 -L error,info -l $LF -P -R rl02 -n
 elif [ "$1" = "medium" ] ; then
-./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -L error,info -l ~/temp/ramdisk/test2.log -d -S 256 -P -R rl02
+./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -L error,info -l $LF -d -S 256 -P -R rl02
 else
-./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -L error,debug -l ~/temp/ramdisk/test2.log -d -t -X -S 256 -P -R rl02
+./build/kek -r work/werkend-mu/unix_v7m_rl0.dsk -r work/werkend-mu/unix_v7m_rl1.dsk -b -L error,debug -l $LF -d -t -X -S 256 -P -R rl02
 fi
