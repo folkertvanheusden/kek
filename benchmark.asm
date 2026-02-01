17c13fdcd79a70bb9cfd1a890b68839cdbe8903b
Folkert van Heusden <folkert@vanheusden.com>
2026-02-01 19:14:31 +0100

    more tests & halt at end

--- benchmark.asm
+++ benchmark.asm
@@ -16,6 +16,24 @@ kw11l_temp:
 	.dw 0
 	.dw 0
 
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+scratch:
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+scratch2:
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+	.dw 0
+
 hex_chars: .ascii "0123456789abcdef"
 value_buffer: .ascii "________________________"
 
@@ -66,7 +84,62 @@ loop:
 	cmp #310,(kw11l_counter)   ; 50 Hz, 4 seconds
 	ble  print_results
 
-	; 
+	; access to registers
+	mov #123, R0
+	add #123, R0
+	sub #123, R0
+	mov R0, R1
+	; SP
+	mov R6, R1
+	SPL 7
+	mov #123, R6
+	add #123, R6
+	sub #123, R6
+	mov R1,R6
+	SPL 0
+	; PC
+	ADD #2, R7
+	NOP
+
+	; shift + div
+	mov #123456, R0
+	ASH #-2,R0
+	mov #1324, R1
+	ASH #1,R1
+	mov #37777, R2
+	div r2,r0
+
+	; mul
+	mov #7,R0
+	mov #17,R1
+	MUL R2,R0
+
+	; compare
+	CMP R0,R1
+	bne bne_hit
+	beq beq_hit
+bne_hit:
+beq_hit:
+
+	; addressing modes
+	mov #400, R5
+addr_move_loop:
+	;mov #scratch2,(#scratch)
+
+	MOV R0, R1     ; 0
+	MOV #scratch,R1
+	MOV R0, (R1)   ; 1
+	MOV R0, (R1)+  ; 2
+	mov R0, @#scratch; 3
+	MOV R0, -(R1)  ; 4
+	MOV R0, -(R1)  ; 4
+	;MOV R0, @-(R0) ; 5  <-- triggers a halt in simh
+	MOV R0, 2(R1)  ; 6
+	MOV R0, #scratch; 7
+
+	DEC R5
+	BNE addr_move_loop
+
 
 	jmp loop
 
@@ -89,7 +162,8 @@ print_results:                     ; print results
 	mov #text_crlf, R2
 	JSR PC,PRINT
 
-finished: jmp finished
+finished: halt
+	jmp finished
 
 make_hex: 
 	movb (R1), R4     ; get first byte to process
