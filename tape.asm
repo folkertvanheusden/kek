b8cb87f05b23616352f75ef98fe1b35f60fbf92a
Folkert van Heusden <folkert@vanheusden.com>
2026-05-08 21:33:49 +0200

    optimized a bit

--- tape.asm
+++ tape.asm
@@ -12,18 +12,12 @@ UNIT0= 000000       ; Unit 0
     mov #2, r1      ; 2 blocks to read
 READ.BLOCK:
     mov r0, @#TMBA  ; Set Memory Address
-    mov @#TMBA,R2
-    clr @#TMCS1
     MOV #-512., @#TMWC ; Set negative byte count
     MOV #UNIT0+READ+GO, @#TMCS1  ; Do read
 
 WAIT.TM:
     BIT #000200, @#TMCS1 ; Bit 7: Ready
     BEQ WAIT.TM     ; If not ready, wait
-
-;    mov @#TMCS1,R2   ; any errors?
-;    mov @#172520,R3  ; what (if any) errors?
-;    halt
 
     add #512.,R0    ; on to the next record
     sob R1, READ.BLOCK
