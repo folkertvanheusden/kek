9a4eb632686f5b3a138e630655853e6d1a16f6c8
Folkert van Heusden <folkert@vanheusden.com>
2026-05-16 14:16:19 +0200

    PICO2W fixes

--- disk_backend.cpp
+++ disk_backend.cpp
@@ -9,7 +9,7 @@
 #if IS_POSIX || defined(_WIN32)
 #include "disk_backend_file.h"
 #endif
-#if defined(ESP32)
+#if defined(ESP32) || defined(BUILD_FOR_PICO2W)
 #include "disk_backend_esp32.h"
 #endif
 #if !defined(BUILD_FOR_PICO2W)
