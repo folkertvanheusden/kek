bc0ea884d30cf5f008b6d4bc31ff689b77832244
Folkert van Heusden <folkert@vanheusden.com>
2026-05-21 14:50:11 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ mkdir logs
 
 (cd build/ && make -j kek-native > ../logs/native.log 2>&1 ; if [ $? -eq 0 ] ; then echo c++ OK ; else echo -e "c++ \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
 (cd build-win32/ && make -j kek-win32 > ../logs/win32.log 2>&1 ; if [ $? -eq 0 ] ; then echo win32 OK ; else echo -e "win32 \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
-(cd ESP32/ && pio pkg update && pio run > ../logs/esp32.log 2>&1 ; if [ $? -eq 0 ] ; then echo esp32 OK ; else echo -e "esp32 \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
-(cd PICO2W/ && pio pkg update && pio run > ../logs/picow.log 2>&1 ; if [ $? -eq 0 ] ; then echo picow OK ; else echo -e "picow \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
-(cd TEENSY4_1/ && pio pkg update && pio run > ../logs/teensy4.1.log 2>&1 ; if [ $? -eq 0 ] ; then echo teensy4.1 OK ; else echo -e "teensy4.1 \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
+(cd ESP32/ && ~/.local/bin/pio pkg update && pio run > ../logs/esp32.log 2>&1 ; if [ $? -eq 0 ] ; then echo esp32 OK ; else echo -e "esp32 \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
+(cd PICO2W/ && ~/.local/bin/pio pkg update && pio run > ../logs/picow.log 2>&1 ; if [ $? -eq 0 ] ; then echo picow OK ; else echo -e "picow \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
+(cd TEENSY4_1/ && ~/.local/bin/pio pkg update && pio run > ../logs/teensy4.1.log 2>&1 ; if [ $? -eq 0 ] ; then echo teensy4.1 OK ; else echo -e "teensy4.1 \e[31mFAIL\e[0m" ; rm -f $TMP ; fi)
 if [ -e $TMP ] ; then
 	echo -e "\e[32mAll GOOD\e[0m"
 	rm -f $TMP
