fab38825efb4f00782afdca5b4c64a6888f49cf6
Folkert van Heusden <folkert@vanheusden.com>
2026-05-23 07:59:11 +0200

    waveshare w5500 support

---
+++ ESP32/eth_transport_esp32.cpp
@@ -0,0 +1,54 @@
+#include "gen.h"
+
+#include "eth_transport_esp32.h"
+#include "log.h"
+#include "utils.h"
+
+
+constexpr const int max_pkt_size = 1512;
+static uint8_t buffer[max_pkt_size];
+
+eth_transport_esp32::eth_transport_esp32(const uint8_t mac[6])
+{
+	memcpy(mac_addr, mac, 6);
+}
+
+eth_transport_esp32::~eth_transport_esp32()
+{
+	delete w5500_instance;
+}
+
+bool eth_transport_esp32::begin()
+{
+	return w5500_instance->begin(mac_addr);
+}
+
+std::string eth_transport_esp32::identifier() const
+{
+	return "esp32+w5500";
+}
+
+void eth_transport_esp32::transmit(const uint8_t *const data, const size_t n_bytes)
+{
+	w5500_instance->sendFrame(data, n_bytes);
+}
+
+std::pair<uint8_t *, size_t> eth_transport_esp32::get(const int timeout)
+{
+	uint8_t *pkt         = nullptr;
+	size_t   packet_size = 0;
+	auto     start       = millis();
+	while(millis() - start < timeout) {
+		int rc = w5500_instance->readFrame(buffer, sizeof buffer);
+		if (rc > 0) {
+			pkt = new uint8_t[rc];
+			if (!pkt) {
+				DOLOG(ll_critical, true, "malloc issue");
+				return { nullptr, 0 };
+			}
+			packet_size = rc;
+			break;
+		}
+	}
+	return { pkt, packet_size };
+}
---
+++ ESP32/eth_transport_esp32.h
@@ -0,0 +1,25 @@
+#include "gen.h"
+#include <cstdint>
+#include <string>
+
+#include "eth_transport.h"
+#include "w5500.h"
+
+
+class eth_transport_esp32: public eth_transport
+{
+private:
+	uint8_t     mac_addr[6]    { };
+	Wiznet5500 *w5500_instance { new Wiznet5500(13, 12, 11, 14) };
+
+public:
+	eth_transport_esp32(const uint8_t mac_addr[6]);
+	virtual ~eth_transport_esp32();
+
+	bool begin() override;
+
+	std::string identifier() const override;
+
+	void transmit(const uint8_t *const data, const size_t n_bytes) override;
+	std::pair<uint8_t *, size_t> get(const int timeout) override;
+};
